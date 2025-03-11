0d976a6da3caa3929b233bc12450a73bd8412127
Manish Katyan <manishkatyan@gmail.com>
2025-03-11 10:28:12 +0530

    updated requirements.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
-from chat import main
-
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+from chat import main
 
 if __name__ == "__main__":
     main()
